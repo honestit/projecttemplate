da4888d690cd8115b4ca7dbffa9d3b8f48bbfbf0
honestit <michal.kupisinski@gmail.com>
2022-05-18 20:10:47 +0200

    Change main class name

--- src/main/java/com/github/honestit/projecttemplate/ProjectTemplateApplication.java
+++ src/main/java/com/github/honestit/projecttemplate/ProjectTemplateApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjecttemplateApplication {
+public class ProjectTemplateApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ProjecttemplateApplication.class, args);
+        SpringApplication.run(ProjectTemplateApplication.class, args);
     }
 
 }
