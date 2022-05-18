10edc2e27c8e7f65e9a4369bc52c086a5468d92b
honestit <michal.kupisinski@gmail.com>
2022-05-18 20:23:34 +0200

    Add validation to entities.

--- src/main/java/com/github/honestit/projecttemplate/model/BookEntity.java
+++ src/main/java/com/github/honestit/projecttemplate/model/BookEntity.java
@@ -3,6 +3,9 @@ package com.github.honestit.projecttemplate.model;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
 import java.util.List;
 
 @Entity
@@ -19,7 +22,9 @@ public class BookEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank
     private String title;
+    @PositiveOrZero
     private int pages;
 
     @ManyToOne
