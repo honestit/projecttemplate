e08238e03205b38e1435d418cacdc7c0654d6bf6
honestit <michal.kupisinski@gmail.com>
2022-05-18 19:57:39 +0200

    Initial commit

---
+++ src/main/java/com/github/honestit/projecttemplate/ProjecttemplateApplication.java
@@ -0,0 +1,13 @@
+package com.github.honestit.projecttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjecttemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjecttemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/honestit/projecttemplate/ProjecttemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.honestit.projecttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjecttemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
