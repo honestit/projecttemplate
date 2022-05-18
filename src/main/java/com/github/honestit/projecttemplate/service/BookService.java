bad0a596af4aa004c8ff2e2f4c6a0526658f691a
honestit <michal.kupisinski@gmail.com>
2022-05-18 20:40:10 +0200

    Add base service interface for managing books with default implementation.

---
+++ src/main/java/com/github/honestit/projecttemplate/service/BookService.java
@@ -0,0 +1,4 @@
+package com.github.honestit.projecttemplate.service;
+
+public interface BookService {
+}
---
+++ src/main/java/com/github/honestit/projecttemplate/service/DefaultBookService.java
@@ -0,0 +1,7 @@
+package com.github.honestit.projecttemplate.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DefaultBookService implements BookService {
+}
