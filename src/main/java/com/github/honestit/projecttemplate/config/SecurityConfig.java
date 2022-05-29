414d4be87f94ba1ece9f93b4bb43b02aee5dcdf3
honestit <michal.kupisinski@gmail.com>
2022-05-29 10:05:02 +0200

    Simplified security config and remove temp class BookBaseData.

--- src/main/java/com/github/honestit/projecttemplate/config/SecurityConfig.java
+++ src/main/java/com/github/honestit/projecttemplate/config/SecurityConfig.java
@@ -14,9 +14,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        // {bcrypt}jlkgja09g8a90bjkoabja09g8a09
-        // {noop}dupa123
-        // {md5}ojgaoigag098g90a
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
@@ -34,22 +31,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/", "/login").permitAll()
-                .antMatchers("/create-book").authenticated()
-                .antMatchers("/create-author").hasRole("ADMIN")
-                .antMatchers("/admin", "/admin/**").hasRole("ADMIN")
-                .and()
-            .csrf()
-                .disable()
-            .formLogin()
-                .loginPage("/login") // domyślny, adres pod którym będzie dostęny formularz logowania, robimy do tego kontroler, który wystawia stornę z formularzem ALE TYLKO NA @GetMapping, nie robimy @PostMapping w ogóle. Formularz ma odesłać żądanie POST na "/login"
-                .usernameParameter("username") // domyślny, nazwa pola w formularzu logowania dla nazwy użytkownika
-                .passwordParameter("password") // domyślny, nazwa pola w formularzu logowania dla hasła
-                .defaultSuccessUrl("/homepage") // strona, na którą trafi użytkownik, jeżeli wszedł bezpośrednio na ścieżkę /login, aby się zalogować
-//                .defaultSuccessUrl("/homepage", true) // wymusza, że po zalogowaniu ZAWSZE trafia się na wskazaną stronę.
-                .and()
-            .logout()
-                .logoutUrl("/logout")
-                .permitAll();
+                .anyRequest().permitAll();
     }
 }
